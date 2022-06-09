22bf18d501d07d72b3036acb4052c5130a5c0dd3
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-09 21:02:02 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide() {
+    return 'divide';
+}
+
+const divide = (parameter1, parameter2) => parameter1 % parameter2;
+divide(2000, 100); //=> 20
+
+const square = (para1, para2) => para1 * para2;
+square(2 * 2); //=> 4
+
+const add = (a + b) => a + b; {
+    let a = 3;
+    let b = 4;
+    console.log(Adding $ { a });
+    console.log(Adding $ { b });
+    return a + b;
+}
+sum(3 + $); //=> 7
